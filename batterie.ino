389f277638a23569f0eceff8a4a0c94b95ba962c
Lukas-Anthony Gutjahr <youdyz.yt@gmail.com>
2025-10-15 14:20:50 +0200

    baterie Check

---
+++ batterie.ino
@@ -0,0 +1,55 @@
+#include <Arduino.h>
+#include <Dezibot.h>
+
+#define BAT_CHG_STAT 39
+#define BAT_ADC 10
+#define BAT_ADC_EN 9
+#define VUSB_SENS 38
+#define FL_PT_EN 37
+
+Dezibot dezibot = Dezibot();
+
+bool getBatChgStat() {
+    if (digitalRead(BAT_CHG_STAT) == HIGH) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+float readBatteryVoltage() {
+    digitalWrite(BAT_ADC_EN, HIGH);
+    delay(10);
+    int adcValue = analogRead(BAT_ADC);
+    digitalWrite(BAT_ADC_EN, LOW);
+    float voltage = (adcValue / 4095.0) * 2 * 3.3;  // Assuming a 12-bit ADC and a voltage divider
+    return voltage;
+}
+
+void init() {
+    //a. GPIO16, GPIO17, GPIO18 als Ausgang, Low geschaltet.
+    pinMode(16, OUTPUT);
+    pinMode(17, OUTPUT);
+    pinMode(18, OUTPUT);
+    digitalWrite(16, LOW);
+    digitalWrite(17, LOW);
+    digitalWrite(18, LOW);
+    // b. GPIO21 als Eingang, ohne interne Pull-Up-/Down-Widerstände.
+    pinMode(21, INPUT);
+}
+
+void setup() {
+    Serial.begin(115200);
+    init();
+
+    if (getBatChgStat()) {
+        Serial.println("High");
+    } else {
+        Serial.println("Low");
+    }
+    Serial.println(readBatteryVoltage());
+}
+
+void loop() {
+    esp_deep_sleep_start();
+}
